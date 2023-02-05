649bc107e888bc63c0b75884398795b71f84a437
WeijieHuang <2632118257@qq.com>
2023-02-05 23:48:44 +0800

    请求封装及md展示

--- code/pages/articles/[id].vue
+++ code/pages/articles/[id].vue
@@ -1,10 +1,27 @@
 <template>
-  <div>hello article</div>
+  <div class="article-content" id="content">
+    <div v-html="content"></div>
+  </div>
 </template>
 
 <script setup>
-let a = '0'
-
+import request from "../../utils/request";
+import { marked } from "marked";
+import { ref, onMounted } from "vue";
+let content = ref("");
+const route = useRoute();
+console.log(route.params.id);
+onMounted(() => {
+  request("/api/articles/" + 1, {}, 0).then((e) => {
+    content.value = e.data.attributes.content;
+    content.value = content.value.replace(
+      "/uploads",
+      "http://127.0.0.1:1337/uploads"
+    );
+    content.value = marked.parse(content.value);
+    console.log(document.getElementsByTagName("h4"));
+  });
+});
 </script>
 <style scoped lang='less'>
 </style>
