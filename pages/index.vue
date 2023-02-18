a773bb519c31438ae1e969a6b9192846a0a3f059
qgxx <qgxx2825313915@outlook.com>
2023-02-19 03:19:49 +0800

    hotRank-dropdown

--- pages/index.vue
+++ pages/index.vue
@@ -9,29 +9,33 @@ const navs = reactive([
     {
         id: 1,
         name: '推荐',
-        type: 0
+        type: 100
     },
     {
         id: 2,
         name: '最新',
-        type: 0
+        type: 200
     },
     {
         id: 3,
         name: '热榜',
-        type: 0,
+        type: 3,
         types: [
             {
-                name: '3天内'
+                name: '3天内',
+                type: 3
             },
             {
-                name: '7天内'
+                name: '7天内',
+                type: 7,
             },
             {
-                name: '30天内'
+                name: '30天内',
+                type: 30,
             },
             {
-                name: '全部'
+                name: '全部',
+                type: 0
             }
         ]
     }
@@ -44,11 +48,11 @@ const navs = reactive([
     <div class="index-wrapper"></div>
       <div class="index-main">
         <div class="main-header">
-          <ul class="main-nav">
-            <li class="main-dev" v-for="nav in navs" :key="nav.name" 
-            :class="[nav.id == index.navId ? 'nav-active' : '']">{{ nav.name }}</li>
-            <el-select v-if="index.navTypes.length" size="mini" style="width: 2em;" placeholder="请选择">
-              <el-option v-for="type in index.navTypes" :label="type.name" :value="item.name"></el-option>
+          <ul class="main-navs">
+            <li class="main-nav" v-for="nav in navs" :key="nav.id" @click="index.changeNavType(nav)"
+            :class="[nav.id === index.navId ? 'nav-active' : '']">{{ nav.name }}</li>
+            <el-select v-if="index.navTypes.length" size="mini" placeholder="请选择" class="hot-rank">
+              <el-option v-for="type in index.navTypes" :label="type.name" :value="type.name"></el-option>
             </el-select>
           </ul>
         </div>
@@ -66,15 +70,39 @@ const navs = reactive([
   }
 }
 .index-main {
+  position: absolute;
+  width: 40%;
+  border: 1px solid #000;
+  border-radius: 3px;
+  background-color: #fff;
+  margin: 0.5em 0 0 20%;
+}
+.main-header {
+  padding: 0.5em 0.75em;
+}
+.main-navs {
+  height: 2em;
+  display: flex;
+  position: relative;
+  align-items: center;
+  .main-nav {
+    display: inline-block;
+    font-size: 0.9em;
+    color: #9e9e9e;
+    cursor: pointer;
+    padding: 0 0.5em;
+  }
+  .hot-rank {
     position: absolute;
-    width: 40%;
-    border: 1px solid #000;
-    margin: 0.5em 0 0 20%;
+    left: 8em;
+    top: 0.2em;
+    width: 1em;
+  }
 }
 .index-side {
-    width: 20%;
-    border: 1px solid #000;
-    margin: 0.5em 18%;
-    float: right;
+  width: 20%;
+  border: 1px solid #000;
+  margin: 0.5em 18%;
+  float: right;
 }
 </style>
--- stores/index.ts
+++ stores/index.ts
@@ -1,28 +1,31 @@
 import { defineStore } from 'pinia'
 
-interface NavTypes {
-    name: string
+interface NavType {
+    name: string,
+    type: number
 }
 
 interface Nav {
     id: number,
     name: string,
     type: number,
-    types: Array<NavTypes>;
+    types?: Array<NavType>
 }
 
 export const useIndexStore = defineStore('index', {
     state: () => {
         return {
             navId: 0,
-            navTypes: [] as NavTypes[]
+            navTypes: [] as Array<NavType>,
+            typesId: 0
         }
     },
     actions: {
         changeNavType(nav: Nav) {
             if (!this.navId && this.navId !== nav.id) {
                 this.navId = nav.id;
-                this.navTypes = nav.types
+                this.navTypes = nav.types || []
+                // console.log('nowNavId: '+ this.navId)
             }
         }
     }
