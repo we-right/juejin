89ae0401730e2db09da66ac003a8a21c019e15d4
qgxx <qgxx2825313915@outlook.com>
2023-02-19 02:17:33 +0800

    toptap-store

--- stores/toptap.ts
+++ stores/toptap.ts
@@ -3,9 +3,9 @@ import { defineStore } from 'pinia'
 export const useTopTapStore = defineStore('toptap', {
     state: () => {
         return {
-            isShowTopTap: false,
-            windowWidth: 0,
-            dropDown: false
+            isShowTopTap: false as boolean,
+            windowWidth: 0 as number,
+            dropDown: false as boolean
         }
     } 
 })
