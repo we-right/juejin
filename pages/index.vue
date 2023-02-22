1bc13d7d025fc40a5ed04b19fc07855e71d78ff9
qgxx <qgxx2825313915@outlook.com>
2023-02-22 14:00:08 +0800

    index-frame

--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,96 @@
 <script setup lang="ts">
-// import Category from ""
-import { useIndexStore } from "../stores/index";
-import { ElSelect, ElOption } from "element-plus";
+import Category from '../components/Category.vue'
+import ArticleList from '../components/ArticleList.vue'
+import { useIndexStore } from '../stores/index'
+import { useTopTapStore } from '~~/stores/toptap'
 
-const index = useIndexStore();
+const index = useIndexStore()
+const toptap = useTopTapStore()
 
+const value = ref('')
 const navs = reactive([
-  {
-    id: 1,
-    name: "推荐",
-    type: 100,
-  },
-  {
-    id: 2,
-    name: "最新",
-    type: 200,
-  },
-  {
-    id: 3,
-    name: "热榜",
-    type: 3,
-    types: [
-      {
-        name: "3天内",
+    {
+        id: 1,
+        name: '推荐',
+        type: 100
+    },
+    {
+        id: 2,
+        name: '最新',
+        type: 200
+    },
+    {
+        id: 3,
+        name: '热榜',
         type: 3,
-      },
-      {
-        name: "7天内",
-        type: 7,
-      },
-      {
-        name: "30天内",
-        type: 30,
-      },
-      {
-        name: "全部",
-        type: 0,
-      },
-    ],
-  },
-]);
+        types: [
+            {
+                value: '3天内',
+                label: '3天内',
+                type: 3
+            },
+            {
+                value: '7天内',
+                label: '7天内',
+                type: 7,
+            },
+            {
+                value: '30天内',
+                label: '30天内',
+                type: 30,
+            },
+            {
+                value: '全部',
+                label: '全部',
+                type: 0
+            }
+        ]
+    }
+])
+
+onMounted(() => {
+  toptap.windowWidth = document.documentElement.clientWidth
+})
 </script>
 
 <template>
   <div class="index">
     <Category />
-    <div class="index-wrapper"></div>
-    <div class="index-main-pc">
-      <div class="main-header">
-        <ul class="main-navs">
-          <li
-            class="main-nav"
-            v-for="nav in navs"
-            :key="nav.id"
-            @click="index.changeNavType(nav)"
-            :class="[nav.id === index.navId ? 'nav-active' : '']"
-          >
-            {{ nav.name }}
-          </li>
-          <el-select
-            v-if="index.navTypes.length"
-            size="small"
-            placeholder="请选择"
-            class="hot-rank"
-          >
-            <el-option
-              v-for="type in index.navTypes"
-              :label="type.name"
-              :value="type.name"
-            ></el-option>
-          </el-select>
-        </ul>
+      <div class="index-main-pc" v-if="toptap.windowWidth >= 1000">
+        <div class="main-header">
+          <ul class="main-navs">
+            <li class="main-nav" v-for="nav in navs" :key="nav.id" @click="index.changeNavType(nav)"
+            :class="[nav.id === index.navId ? 'nav-active' : '']">{{ nav.name }}</li>
+
+            <el-select v-if="index.navTypes.length" size="small" placeholder="请选择" class="hot-rank" v-model="value" style="width: 5em;">
+              <el-option v-for="type in index.navTypes" :label="type.label" :value="type.value" :key="type.value"></el-option>
+            </el-select>
+          
+          </ul>
+        </div>
+        <article-list></article-list>
       </div>
-    </div>
-    <div class="index-side">小册</div>
-    <div>
-      <article-card></article-card>
-    </div>
+      <div class="index-main-mobile" v-else>
+        <div class="main-header">
+          <ul class="main-navs">
+            <li class="main-nav" v-for="nav in navs" :key="nav.id" @click="index.changeNavType(nav)"
+            :class="[nav.id === index.navId ? 'nav-active' : '']">{{ nav.name }}</li>
+
+            <el-select v-if="index.navTypes.length" size="small" placeholder="请选择" class="hot-rank" v-model="value" style="width: 5em;">
+              <el-option v-for="type in index.navTypes" :label="type.label" :value="type.value" :key="type.value"></el-option>
+            </el-select>
+
+          </ul>
+        </div>
+        <article-list></article-list>
+      </div>
+      <div class="index-side" v-if="toptap.windowWidth > 1000">小册</div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .index {
   position: relative;
-  .index-wrapper {
-    position: absolute;
-    top: 4em;
-  }
 }
 .index-main-pc {
   position: absolute;
@@ -95,6 +99,13 @@ const navs = reactive([
   border-radius: 3px;
   background-color: #fff;
   margin: 0.5em 0 0 20%;
+}
+.index-main-mobile {
+  position: absolute;
+  left: 0;
+  width: 100%;
+  border-bottom: #9e9e9e;
+  background-color: #fff;
 }
 .main-header {
   padding: 0.5em 0.75em;
@@ -115,7 +126,7 @@ const navs = reactive([
     position: absolute;
     left: 8em;
     top: 0.2em;
-    width: 1em;
+    border: none;
   }
 }
 .index-side {
